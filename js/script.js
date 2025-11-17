e11b2f503f2f63b6d4ed9677f4834000fab89489
thomaslinux <thomasdanger2001@gmail.com>
2025-11-17 14:02:21 +0100

    Création des boutons de navigation

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
+import { entierAleatoire } from "tl9_global.js";
+
 console.log("Script chargé")
+
+
+/**
+ * Une fonction pour initialiser le jeu
+ */
+function memoryInit() {
+    
+}
---
+++ js/tl9_global.js
@@ -0,0 +1,8 @@
+/**
+ * @param {Number} MIN valeur minimum du nombre aléatoire (inclue)
+ * @param {Number} MAX valeur maximum du nombre aléatoire (excluse)
+ * @returns {Number} un entier aléatoire entre une valeur MIN (inclue) et une valeur MAX (exclue)
+ */
+export function entierAleatoire(MIN,MAX) {
+    return Math.floor(Math.random()*(MAX-MIN)+ MIN);
+}
