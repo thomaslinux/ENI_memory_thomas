46bb15810d987a0892bd9cb1be231a94c6cd2d4f
thomaslinux <thomasdanger2001@gmail.com>
2025-11-19 14:58:25 +0100

    placer des animaux aléatoirement dans un tableau de 16 cases

--- js/memory.js
+++ js/memory.js
@@ -1,5 +1,3 @@
-console.log(entierAleatoire(10, 20));
-
 /**
  * @param {Number} MIN valeur minimum du nombre aléatoire (inclue)
  * @param {Number} MAX valeur maximum du nombre aléatoire (excluse)
@@ -9,20 +7,47 @@ function entierAleatoire(MIN, MAX) {
     return Math.floor(Math.random() * (MAX - MIN) + MIN);
 }
 
-var plateau = new Array(16);
-
-console.log(plateau);
 /**
  * NIVEAU 1 : Fonction d'initialisation
  */
 $(function () {
     //initialise le tableau des cases vides
-    plateau.fill(0)
     //associe un évènement à tous les td
     $('img').attr("src", "ressources/question.svg")
     $('img').click(imageReplace);
 });
+console.log("ici")
+let tab = new Array(16);
+tab.fill(0);
+console.log(tab);
+tab = tableauPaires(tab);
+console.log(tab);
+tab = melangeTab(tab);
+console.log(tab);
 
+/**
+ * Rempli un tableau de 0 à sa taille par 2 fois les nombres.
+ */
+function tableauPaires(tab) {
+    let longueur = tab.length;
+    for (i=0;i<longueur;i++) {
+        tab[i]=i;
+        tab[longueur-1-i]=i;
+    }
+    return tab;
+}
+
+/**
+ * Mélange un tableau, fonction récupérée sur Wikipedia, noms modifiés
+ * https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle#JavaScript_implementation
+ */
+function melangeTab(tab) {
+    for (let i = tab.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [tab[i], tab[j]] = [tab[j], tab[i]]; // échange la case i et j
+    }
+    return tab;
+}
 
 /**
  * 
@@ -31,27 +56,9 @@ $(function () {
  */
 function imageReplace(e) {
     //Met la croix dans l'image dans la zone cliquée.
-    $(e.target).attr("src", "ressources/animaux/1.webp");
+    $(e.target).attr("src", "ressources/animaux/" + tab[$(e.target).attr("id")] + ".webp");
+    console.log($(e.target).attr("id")+1);
+    // $(e.target).attr("src", "ressources/animaux/1.webp");
     //Supprime l'évènement
     $(e.currentTarget).off("click");
-}
-
-
-/**
- * Jusqu'à ce que le tableau soit rempli, 
- * place deux nombres identiques dans deux cases différentes.
- */
-function rempliPlateau(nbCartes) {
-
-    for (i=0;i<plateau.length;) {
-        const nombreAPlacer = entierAleatoire(1,nbCartes);
-        let nbPlacements = 0; 
-        while (nbPlacements<2) {
-            let casePlateau = entierAleatoire(1,plateau.length());
-            if (plateau[casePlateau]==0) { // si la case est vide
-                plateau[casePlateau] = nombreAPlacer;
-
-            }
-        }
-    }
 }
